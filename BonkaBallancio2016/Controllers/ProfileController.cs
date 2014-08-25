fbb4c3ae50cf86bde3e17cb87c9bd95955a7c027
Ballance <ballance@gmail.com>
2014-08-25 08:57:41 -0400

    Signed-off-by: Ballance <ballance@gmail.com>

---
+++ BonkaBallancio2016/Controllers/ProfileController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Web.Mvc;
+using BonkaBallancio.Models.ViewModels;
+
+namespace BonkaBallancio.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string Id)
+        {
+            ProfileViewModel profile = null;
+            switch (Id)
+            {
+                case "Ballancio":
+                    {
+                        profile = BallancioProfile();
+                    break;
+                    }
+                case "Bonka":
+                {
+                    profile = BonkaProfile();
+                    break;
+                }
+                default:
+                    throw new NotImplementedException("Nobody by that name on the ticket");
+            }
+            return View(profile);
+        }
+
+         public ActionResult Bonka()
+        {
+             return View(BonkaProfile());
+             }
+
+         public ActionResult Ballancio()
+         {
+             return View(BallancioProfile());
+         }
+
+        public ProfileViewModel BonkaProfile()
+        {
+            var pro = new ProfileViewModel()
+            {
+                Name = "Borka",
+                Bio = "Wanders the earth.",
+                HomeTown = "Elkton, KY"
+            };
+
+            return pro;
+        }
+        public ProfileViewModel BallancioProfile()
+        {
+            var pro = new ProfileViewModel()
+            {
+                Name = "Ballancio",
+                Bio = "His shoes fell off.",
+                HomeTown = "Greensboro, NC"
+            };
+
+            return pro;
+        }
+    }
+}
---
+++ BonkaBallancio2016/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BonkaBallancio.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+
+        public string HomeTown { get; set; }
+    }
+}
